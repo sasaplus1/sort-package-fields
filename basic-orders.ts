71bb37d71bb6952014530feb8384439435c4c748
sasaplus1 <sasaplus1@gmail.com>
2022-07-25 23:33:22 +0900

    add basic-orders.ts

---
+++ basic-orders.ts
@@ -0,0 +1,324 @@
+export const basicOrders = {
+  name: {
+    order: 0
+  },
+  version: {
+    order: 1
+  },
+  description: {
+    order: 2
+  },
+  keywords: {
+    order: 3
+  },
+  homepage: {
+    order: 4
+  },
+  bugs: {
+    order: 5,
+    properties: {
+      url: {
+        order: 0
+      },
+      email: {
+        order: 1
+      }
+    }
+  },
+  license: {
+    order: 6
+  },
+  licenses: {
+    order: 7
+  },
+  author: {
+    order: 8
+  },
+  contributors: {
+    order: 9
+  },
+  maintainers: {
+    order: 10
+  },
+  files: {
+    order: 11
+  },
+  main: {
+    order: 12
+  },
+  exports: {
+    order: 13
+  },
+  bin: {
+    order: 14
+  },
+  type: {
+    order: 15
+  },
+  types: {
+    order: 16
+  },
+  typings: {
+    order: 17
+  },
+  typesVersions: {
+    order: 18
+  },
+  man: {
+    order: 19
+  },
+  directories: {
+    order: 20,
+    properties: {
+      bin: {
+        order: 0
+      },
+      doc: {
+        order: 1
+      },
+      example: {
+        order: 2
+      },
+      lib: {
+        order: 3
+      },
+      man: {
+        order: 4
+      },
+      test: {
+        order: 5
+      }
+    }
+  },
+  repository: {
+    order: 21,
+    properties: {
+      type: {
+        order: 0
+      },
+      url: {
+        order: 1
+      },
+      directory: {
+        order: 2
+      }
+    }
+  },
+  funding: {
+    order: 22
+  },
+  scripts: {
+    order: 23,
+    properties: {
+      lint: {
+        order: 0
+      },
+      prepublish: {
+        order: 1
+      },
+      prepare: {
+        order: 2
+      },
+      prepublishOnly: {
+        order: 3
+      },
+      prepack: {
+        order: 4
+      },
+      postpack: {
+        order: 5
+      },
+      publish: {
+        order: 6
+      },
+      postpublish: {
+        order: 7
+      },
+      preinstall: {
+        order: 8
+      },
+      install: {
+        order: 9
+      },
+      postinstall: {
+        order: 10
+      },
+      preuninstall: {
+        order: 11
+      },
+      uninstall: {
+        order: 12
+      },
+      postuninstall: {
+        order: 13
+      },
+      preversion: {
+        order: 14
+      },
+      version: {
+        order: 15
+      },
+      postversion: {
+        order: 16
+      },
+      pretest: {
+        order: 17
+      },
+      test: {
+        order: 18
+      },
+      posttest: {
+        order: 19
+      },
+      prestop: {
+        order: 20
+      },
+      stop: {
+        order: 21
+      },
+      poststop: {
+        order: 22
+      },
+      prestart: {
+        order: 23
+      },
+      start: {
+        order: 24
+      },
+      poststart: {
+        order: 25
+      },
+      prerestart: {
+        order: 26
+      },
+      restart: {
+        order: 27
+      },
+      postrestart: {
+        order: 28
+      },
+      serve: {
+        order: 29
+      }
+    }
+  },
+  config: {
+    order: 24
+  },
+  dependencies: {
+    order: 25
+  },
+  devDependencies: {
+    order: 26
+  },
+  optionalDependencies: {
+    order: 27
+  },
+  peerDependencies: {
+    order: 28
+  },
+  peerDependenciesMeta: {
+    order: 29
+  },
+  bundledDependencies: {
+    order: 30
+  },
+  bundleDependencies: {
+    order: 31
+  },
+  resolutions: {
+    order: 32
+  },
+  packageManager: {
+    order: 33
+  },
+  engines: {
+    order: 34,
+    properties: {
+      node: {
+        order: 0
+      }
+    }
+  },
+  engineStrict: {
+    order: 35
+  },
+  os: {
+    order: 36
+  },
+  cpu: {
+    order: 37
+  },
+  preferGlobal: {
+    order: 38
+  },
+  private: {
+    order: 39
+  },
+  publishConfig: {
+    order: 40,
+    properties: {
+      access: {
+        order: 0
+      },
+      tag: {
+        order: 1
+      },
+      registry: {
+        order: 2
+      }
+    }
+  },
+  dist: {
+    order: 41,
+    properties: {
+      shasum: {
+        order: 0
+      },
+      tarball: {
+        order: 1
+      }
+    }
+  },
+  readme: {
+    order: 42
+  },
+  module: {
+    order: 43
+  },
+  esnext: {
+    order: 44,
+    properties: {
+      main: {
+        order: 0
+      },
+      browser: {
+        order: 1
+      }
+    }
+  },
+  workspaces: {
+    order: 45
+  },
+  jspm: {
+    order: 46
+  },
+  eslintConfig: {
+    order: 47
+  },
+  prettier: {
+    order: 48
+  },
+  stylelint: {
+    order: 49
+  },
+  ava: {
+    order: 50
+  },
+  release: {
+    order: 51
+  },
+  jscpd: {
+    order: 52
+  }
+} as const;
